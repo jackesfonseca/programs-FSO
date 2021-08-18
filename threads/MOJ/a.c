85500add60e400ad3c8f06989a2468d834049785
jackesfonseca <jackesdafonseca@gmail.com>
2021-08-18 17:23:09 -0300

    working on threads

--- threads/MOJ/a.c
+++ threads/MOJ/a.c
@@ -1,3 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <>
+#include <pthread.h>
+
+int compare_data(const void *a, const void *b) {
+	printf("Compara...\n");
+	return *((int *)a) - *((int *)b);
+}
+
+void *sort_data(void *vet) {
+	printf("Thread...\n");
+	qsort((int *)vet, sizeof((int *)vet)/sizeof(int), sizeof(int), compare_data);
+}
+
+int main(void) {
+
+	pthread_t threads[2];
+	int len, i;
+	int *vet1, *vet2;
+
+	scanf("%d", &len);
+
+	vet1 = malloc(sizeof(int)*len);
+	vet2 = malloc(sizeof(int)*len);
+
+	//get first vector data 
+	for(i=0; i<len; i++)
+		scanf("%d", &vet1[i]);
+
+	pthread_create(&threads[0], NULL, &sort_data, (void *)vet1);
+
+	pthread_join(threads[0], NULL);
+
+	//get second vector data
+	for(i=0; i<len; i++)
+		scanf("%d", &vet2[i]);
+
+	pthread_create(&threads[1], NULL, &sort_data, (void *)vet2);
+
+	pthread_join(threads[1], NULL);
+
+	for(i=0; i<len; i++)
+		printf("%d", vet2[i]);
+	if(i != len-1)
+		printf(" ");
+
+	printf("\n");
+}
---
+++ threads/thread3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(void) {
+	pthread_t threads[2];
+
+	
+}
