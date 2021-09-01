c6fabc1a02944c89c633a2cad32a981c82bd199b
jackesfonseca <jackesdafonseca@gmail.com>
2021-09-01 17:32:43 -0300

    working on mini shell

--- Trabalho 1/mini_shell.c
+++ Trabalho 1/mini_shell.c
@@ -10,8 +10,9 @@
 
 int main()
 {
-	int seconds, micros, seconds_final, micros_final, ret;
 	struct timeval start, end, start_final, end_final;
+	double program_time, command_time;
+	int ret;
 	pid_t child_process;
 	char *arg0, *arg1, *path, *path_copy;
 
@@ -34,39 +35,37 @@ int main()
 		arg0 = strtok(path_copy, "/");
 		arg0 = strtok(NULL, "/");
 
+		/* get command start time */
+		gettimeofday(&start, NULL);
+
 		/* if it's a child process */
 		if(fork() == 0)
 		{
-			/* get command start time */
-			gettimeofday(&start, NULL);
 
 			/* execute command */
 			execl(path, arg0, arg1, NULL);
 
-			/* command end time */
-			gettimeofday(&end, NULL);
-
-			/* calculate command execution time in seconds and micros */
-			seconds = (end.tv_sec - start.tv_sec);
-			micros  = ((end.tv_usec/1000) - (start.tv_usec)/1000);
 		}
+
+		/* command end time */
+		gettimeofday(&end, NULL);
+
+		/* calculate command execution time in seconds and micros */
+		command_time = end.tv_sec + end.tv_usec/1000000 - start.tv_sec + start.tv_usec/1000000;
 
 		/* get child process status */
 		child_process = wait(&ret);
 
 		/* print result*/
-		printf("> Demorou %d.%d segundos retornou %d\n", seconds, micros, ret);
+		printf("> Demorou %.1lf segundos retornou %d\n", command_time, WEXITSTATUS(ret));
 	}
 
-	/* get final end time*/
-	gettimeofday(&end_final, NULL);
-
 	/* calculate final execution time in seconds and micros */
-	seconds = (end_final.tv_sec - start_final.tv_sec);
-	micros  = ((end_final.tv_usec/1000) - (start_final.tv_usec)/1000);
+	program_time = end_final.tv_sec + end_final.tv_usec/1000000 
+							- start_final.tv_sec + start_final.tv_usec/1000000;
 
 	/* print result */
-	printf(">> O tempo total foi de %d.%d segundos\n", seconds_final, micros_final);
+	printf(">> O tempo total foi de %.1lf segundos\n", program_time);
 
 	return 0;
 }
